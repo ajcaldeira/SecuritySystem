7ee62db252f62d01421d46d78de5f4a4b9db1bc9
Angelo <angelo.josey@gmail.com>
2020-02-02 15:34:17 +0000

    fix query

--- writeDB.py
+++ writeDB.py
@@ -5,7 +5,7 @@ def WriteImage(img_data,date):
     con = pymysql.connect('localhost', 'root', return_pass.GetPass(), 'security')
     with con:    
         cur = con.cursor() 
-        cur.execute("INSERT INTO security (image, date) VALUES (HEX(AES_ENCRYPT(%s,'IlvdPuXqiNUa1ONh1V7HOAr1pRBbI7rh')),%s)", 
+        cur.execute("INSERT INTO images (image, date) VALUES (HEX(AES_ENCRYPT(%s,'IlvdPuXqiNUa1ONh1V7HOAr1pRBbI7rh')),%s)", 
             (img_data, date))    
         con.commit()
         con.close()
