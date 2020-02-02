a5b1341bd337fe7e0289edf1444d41bf0b0133b4
Angelo <angelo.josey@gmail.com>
2020-02-02 15:29:56 +0000

    changed db lib to support python 3

--- writeDB.py
+++ writeDB.py
@@ -1,10 +1,12 @@
 #DOCS: http://zetcode.com/db/mysqlpython/
-import MySQLdb as mdb
+import pymysql
 import return_pass
 def WriteImage(img_data,date):
-    con = mdb.connect('localhost', 'root', return_pass.GetPass(), 'security')
+    con = pymysql.connect('localhost', 'root', return_pass.GetPass(), 'security')
     with con:    
         cur = con.cursor() 
         cur.execute("INSERT INTO security (image, date) VALUES (HEX(AES_ENCRYPT(%s,'IlvdPuXqiNUa1ONh1V7HOAr1pRBbI7rh')),%s)", 
             (img_data, date))    
+        con.commit()
+        con.close()
         print("Number of rows updated:",  cur.rowcount)
