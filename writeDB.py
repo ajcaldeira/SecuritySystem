71b48a37f8c426eae3460b075f6cfde82df20a24
Angelo <angelo.josey@gmail.com>
2020-02-02 15:24:52 +0000

    syntax fix

--- writeDB.py
+++ writeDB.py
@@ -7,4 +7,4 @@ def WriteImage(img_data,date):
         cur = con.cursor() 
         cur.execute("INSERT INTO security (image, date) VALUES (HEX(AES_ENCRYPT(%s,'IlvdPuXqiNUa1ONh1V7HOAr1pRBbI7rh')),%s)", 
             (img_data, date))    
-        print "Number of rows updated:",  cur.rowcount
+        print("Number of rows updated:",  cur.rowcount)
