aaedc93c651ebd2b8162977d4123d069a1891edb
Angelo <angelo.josey@gmail.com>
2020-04-07 12:08:11 +0100

    adding func

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from simple import *
+
 
 def main():
   print("OK")
+  RunCam()
+  #server starts already by importing simple.py
+  #
   
 if __name__== "__main__":
   main()
+
+
+def RunCam():
+  from simple import *
