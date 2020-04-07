d48784455adf19943c1ce47a221ee348ef8b0ede
Angelo <angelo.josey@gmail.com>
2020-04-07 17:16:20 +0100

    added kill all script

---
+++ kill_all.py
@@ -0,0 +1,7 @@
+os.system("pkill -9 -f ultrasonic.py")
+os.system("pkill -9 -f main.py")
+os.system("pkill -9 -f simple.py")
+os.system("pkill -9 -f writeDB.py")
+os.system("pkill -9 -f process_image.py")
+os.system("pkill -9 -f buzzer.py")
+os.system("pkill -9 -f parse.py")
