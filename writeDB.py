a9afd7b408765fc9091a399c0916f209007a9e98
Angelo <angelo.josey@gmail.com>
2020-04-07 11:53:58 +0100

    ENV VARS

--- writeDB.py
+++ writeDB.py
@@ -1,11 +1,15 @@
 #DOCS: http://zetcode.com/db/mysqlpython/
 import pymysql
 import return_pass
+import os
+import env
+ENC_KEY = os.getenv('ENC_KEY')
 def WriteImage(img_data,date):
     con = pymysql.connect('localhost', 'root', return_pass.GetPass(), 'security')
     with con:    
         cur = con.cursor() 
-        cur.execute("INSERT INTO images (image, date) VALUES (HEX(AES_ENCRYPT(%s,'IlvdPuXqiNUa1ONh1V7HOAr1pRBbI7rh')),%s)", 
-            (img_data, date))    
+        
+        cur.execute("INSERT INTO images (image, date) VALUES (HEX(AES_ENCRYPT(%s,'" + ENC_KEY + "')),%s)", 
+            (img_data, date))
         con.commit()
         cur.close()
