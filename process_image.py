bcea9735c5a2159c93432098b255327a98bc0aa2
Angelo <angelo.josey@gmail.com>
2020-04-08 21:31:26 +0100

    SAME

--- process_image.py
+++ process_image.py
@@ -2,9 +2,9 @@ from base64 import b64encode
 import sys
 import writeDB
 import os
-def ProcessImage(IMG_NAME):
-    newStr = b64encode(os.path.join(WRITE_DIR,IMG_NAME))
-    writeDB.WriteImage(newStr,IMG_NAME[:-4])
+def ProcessImage(IMG_LOC):
+    newStr = b64encode(IMG_LOC)
+    writeDB.WriteImage(newStr,IMG_LOC[:-4])
     # with open(IMG_NAME, "rb") as img_file:
     #     img_str = base64.b64encode(img_file.read())
     #     img_str = img_str[2:-1]
