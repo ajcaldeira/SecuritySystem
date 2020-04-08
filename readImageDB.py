474663a5882c11a7151d6da2813c0c929d0c78f2
Angelo <angelo.josey@gmail.com>
2020-04-08 20:58:43 +0100

    printing things

--- readImageDB.py
+++ readImageDB.py
@@ -19,6 +19,7 @@ def ReadImage(img_date):
         rows = cur.fetchall()
         for row in rows:
             imgdata = base64.b64decode(str(row[0]))#this is the decided b64 img
+            print(str(row[0]))
             with open(img_date, 'wb') as f:
                 f.write(os.path.join(WRITE_DIR,imgdata, ".png"))
                 print(os.path.join(WRITE_DIR,imgdata, ".png"))
