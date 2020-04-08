18c5af0e40ab76479c0c6870c84110f66fb93e85
Angelo <angelo.josey@gmail.com>
2020-04-08 21:30:29 +0100

    same

--- process_image.py
+++ process_image.py
@@ -1,9 +1,9 @@
-import base64
+from base64 import b64encode
 import sys
 import writeDB
 import os
 def ProcessImage(IMG_NAME):
-    newStr = base64.base64encode(os.path.join(WRITE_DIR,IMG_NAME))
+    newStr = b64encode(os.path.join(WRITE_DIR,IMG_NAME))
     writeDB.WriteImage(newStr,IMG_NAME[:-4])
     # with open(IMG_NAME, "rb") as img_file:
     #     img_str = base64.b64encode(img_file.read())
