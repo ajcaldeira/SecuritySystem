f492a08f60f60e291dd31f5c4924b418cdb47cd9
Angelo <angelo.josey@gmail.com>
2020-04-08 21:43:48 +0100

    bug in writing

--- process_image.py
+++ process_image.py
@@ -4,7 +4,7 @@ import writeDB
 import os
 def ProcessImage(IMG_LOC,IMG_NAME):
     newStr = b64encode(str(IMG_LOC).encode())
-    writeDB.WriteImage(newStr,IMG_NAME)
+    writeDB.WriteImage(newStr,IMG_NAME[:-4])
     # with open(IMG_NAME, "rb") as img_file:
     #     img_str = base64.b64encode(img_file.read())
     #     img_str = img_str[2:-1]
