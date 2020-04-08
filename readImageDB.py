93c7aca955af7d0e69cbe7922d7b176a681c8bee
Angelo <angelo.josey@gmail.com>
2020-04-08 21:42:07 +0100

    reading now

--- readImageDB.py
+++ readImageDB.py
@@ -5,24 +5,19 @@ import pymysql
 import return_pass
 import os
 import env
-import base64
+from base64 import b64decode
 ENC_KEY = os.getenv('ENC_KEY')
 DB_PASS = os.getenv('DB_PASS')
-WRITE_DIR = '/var/www/html/ThesisMobileApp/temp_img'
 def ReadImage(img_date):
-    #print(img_date)
     con = pymysql.connect('localhost', 'root', DB_PASS, 'security')
     with con:
-        print("SELECT AES_DECRYPT(unhex(image),'" + ENC_KEY + "') AS decImg from images WHERE date =")
         cur = con.cursor() 
         cur.execute("SELECT AES_DECRYPT(unhex(image),'" + ENC_KEY + "') AS decImg from images WHERE date = %s", 
             (img_date))
         rows = cur.fetchall()
         for row in rows:
-            imgdata = base64.b64decode(str(row[0]))#this is the decided b64 img
-            with open(img_date, 'wb') as f:
-                f.write(os.path.join(WRITE_DIR,imgdata, ".png"))
-                print(os.path.join(WRITE_DIR,imgdata, ".png"))
+            imgdata = b64decode(str(row[0]))#this is the decoded b64 img loc
+            print(imgdata)
             
              
         cur.close()
