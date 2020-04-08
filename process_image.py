1d4c826a191a6324c64c4b1f5eeff67ea55ccbfa
Angelo <angelo.josey@gmail.com>
2020-04-08 21:15:10 +0100

    saving image loc
    saving image on server instead

--- process_image.py
+++ process_image.py
@@ -2,7 +2,9 @@ import base64
 import sys
 import writeDB
 def ProcessImage(IMG_NAME):
-    with open(IMG_NAME, "rb") as img_file:
-        img_str = base64.b64encode(img_file.read())
-        img_str = img_str[2:-1]
-        writeDB.WriteImage(img_str,IMG_NAME[:-4])
+    newStr = base64.base64encode(os.path.join(WRITE_DIR,IMG_NAME))
+    writeDB.WriteImage(newStr,IMG_NAME[:-4])
+    # with open(IMG_NAME, "rb") as img_file:
+    #     img_str = base64.b64encode(img_file.read())
+    #     img_str = img_str[2:-1]
+    #     writeDB.WriteImage(img_str,IMG_NAME[:-4])
--- screenshot.py
+++ screenshot.py
@@ -10,9 +10,9 @@ import subprocess
 import time
 
 def main():
-    directory = '/home/pi/Desktop/SecuritySystem/'
+    # directory = '/home/pi/Desktop/SecuritySystem/'
     stream = urllibr.urlopen('http://localhost:1654/stream.mjpg')
-    
+    WRITE_DIR = '/var/www/html/ThesisMobileApp/temp_img'
     bytes=b''
     while True:
         bytes += stream.read(1024)
@@ -24,8 +24,8 @@ def main():
             i = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
             gray = cv2.cvtColor(i,cv2.COLOR_BGR2GRAY)
             IMG_NAME = str(datetime.now().timestamp())+ '.png' # time object
-            cv2.imwrite(IMG_NAME,i)
-            process_image.ProcessImage(IMG_NAME)
+            cv2.imwrite(os.path.join(WRITE_DIR,IMG_NAME),i)
+            process_image.ProcessImage(os.path.join(WRITE_DIR,IMG_NAME))
             #print(IMG_NAME[:-4])
             return 1
 
