c153e48df31b8db3b13a444e4149d4ba09067178
Angelo <angelo.josey@gmail.com>
2020-04-09 23:50:39 +0100

    fancy email

--- send_email.py
+++ send_email.py
@@ -1,5 +1,7 @@
 #SOURCED FROM: https://realpython.com/python-send-email/
 import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 import os
 import env
 
@@ -11,23 +13,45 @@ def SendEmailNotification(img_url):
     sender_email = EMAIL_USER
     receiver_email = "angelo.josey@gmail.com" #pull this from the db later
     password = EMAIL_PASS
-    message = """\
-    Subject: Snapshot taken!
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "PiSecurity"
+    message["From"] = sender_email
+    message["To"] = receiver_email
 
-    PiSecurity has taken a snapshot!
-    You can delete this from the app if you wish.
-
-    <img src='https://puu.sh/Fvs83/9d9c6852b1.png'>
-    
+    # Create the plain-text and HTML version of your message
+    text = """\
+    Hi,
+    How are you?
+    Real Python has many great tutorials:
+    www.realpython.com"""
+    html = """\
+    <html>
+    <body>
+        <p>Hi,<br>
+        How are you?<br>
+        <a href="https://puu.sh/Fvs83/9d9c6852b1.png">Real Python</a> 
+        has many great tutorials.
+        </p>
+    </body>
+    </html>
     """
 
+    # Turn these into plain/html MIMEText objects
+    part1 = MIMEText(text, "plain")
+    part2 = MIMEText(html, "html")
+
+    # Add HTML/plain-text parts to MIMEMultipart message
+    # The email client will try to render the last part first
+    message.attach(part1)
+    message.attach(part2)
+
+    # Create secure connection with server and send email
     context = ssl.create_default_context()
-    with smtplib.SMTP(smtp_server, port) as server:
-        server.ehlo()  # Can be omitted
-        server.starttls(context=context)
-        server.ehlo()  # Can be omitted
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
         server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message)
+        server.sendmail(
+            sender_email, receiver_email, message.as_string()
+        )
 
 if __name__== "__main__":
-    SendEmailNotification("https://puu.sh/Fvs83/9d9c6852b1.png".decode('utf-8'))
+    SendEmailNotification("https://puu.sh/Fvs83/9d9c6852b1.png")
