25be7c1e71bfa4dcc44fefc1254051a9549d2c0f
Angelo <angelo.josey@gmail.com>
2020-04-10 00:07:50 +0100

    Added img and time to email test 1

--- screenshot.py
+++ screenshot.py
@@ -8,8 +8,9 @@ from datetime import datetime
 import os
 import subprocess
 import time
-
+import send_email
 def main():
+    BASE_IMG_URL = os.getenv('BASE_IMG_URL')
     # directory = '/home/pi/Desktop/SecuritySystem/'
     stream = urllibr.urlopen('http://localhost:1654/stream.mjpg')
     WRITE_DIR = '/var/www/html/ThesisMobileApp/temp_img'
@@ -23,9 +24,12 @@ def main():
             bytes = bytes[b+2:]
             i = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
             gray = cv2.cvtColor(i,cv2.COLOR_BGR2GRAY)
-            IMG_NAME = str(datetime.now().timestamp())+ '.png' # time object
+            dateNow = datetime.now().timestamp()
+            IMG_NAME = str(dateNow)+ '.png' # time object
             cv2.imwrite(os.path.join(WRITE_DIR,IMG_NAME),i)
             process_image.ProcessImage(os.path.join(WRITE_DIR,IMG_NAME),IMG_NAME)
+            full_url = str(BASE_IMG_URL) + str(IMG_NAME)
+            send_email.SendEmailNotification(full_url,datetime.fromtimestamp(dateNow))
             return 1
 
 if __name__== "__main__":
--- send_email.py
+++ send_email.py
@@ -1,11 +1,12 @@
 #SOURCED FROM: https://realpython.com/python-send-email/
+#ADDITIONAL INFORMATION: https://hostpresto.com/community/tutorials/how-to-send-email-from-the-command-line-with-msmtp-and-mutt/
 import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import os
 import env
 
-def SendEmailNotification(img_url):
+def SendEmailNotification(img_url,time_now):
     EMAIL_USER = os.getenv('EMAIL_USER')
     EMAIL_PASS = os.getenv('EMAIL_PASS')
     port = 587  # For starttls
@@ -17,29 +18,24 @@ def SendEmailNotification(img_url):
     message["Subject"] = "PiSecurity"
     message["From"] = sender_email
     message["To"] = receiver_email
-
     # Create the plain-text and HTML version of your message
-    text = """\
-    Hi,
-    How are you?
-    Real Python has many great tutorials:
-    www.realpython.com"""
+    text = f"""\
+    Sent From PiSecurity at {time_now}"""
     html = f"""\
     <html>
     <body>
-        <p>Hi,<br>
-        How are you?<br>
-        <a href="https://puu.sh/Fvs83/9d9c6852b1.png">Real Python</a>
+        <h2>PiSecurity has taken a screenshot!</h2>
+        <h3>You can view or this on the app</h3>
+        <br>
         <img src="{img_url}">
-        has many great tutorials.
-        </p>
+        <br>
     </body>
     </html>
     """
 
     # Turn these into plain/html MIMEText objects
-    part1 = MIMEText(text, "plain")
-    part2 = MIMEText(html, "html")
+    part2 = MIMEText(text, "plain")
+    part1 = MIMEText(html, "html")
 
     # Add HTML/plain-text parts to MIMEMultipart message
     # The email client will try to render the last part first
@@ -53,6 +49,3 @@ def SendEmailNotification(img_url):
         server.sendmail(
             sender_email, receiver_email, message.as_string()
         )
-
-if __name__== "__main__":
-    SendEmailNotification("https://puu.sh/Fvs83/9d9c6852b1.png")
