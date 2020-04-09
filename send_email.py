a0de6e7902d28ce138ae7273806d1d820d47397e
Angelo <angelo.josey@gmail.com>
2020-04-09 23:35:27 +0100

    email sender

---
+++ send_email.py
@@ -0,0 +1,30 @@
+#SOURCED FROM: https://realpython.com/python-send-email/
+import smtplib, ssl
+import os
+import env
+
+def SendEmailNotification(img_url):
+    EMAIL_USER = os.getenv('EMAIL_USER')
+    EMAIL_PASS = os.getenv('EMAIL_PASS')
+    port = 587  # For starttls
+    smtp_server = "smtp.gmail.com"
+    sender_email = EMAIL_USER
+    receiver_email = "angelo.josey@gmail.com" #pull this from the db later
+    password = EMAIL_PASS
+    message = f"""\
+    Subject: Snapshot taken!
+
+    PiSecurity has taken a snapshot!
+    You can delete this from the app if you wish.
+
+    <img src="{img_url}">
+    
+    """
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.ehlo()  # Can be omitted
+        server.starttls(context=context)
+        server.ehlo()  # Can be omitted
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
